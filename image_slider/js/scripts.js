9e18efac57000eef8887912688868a6c0501f1ce
Chris Davis <chris.davis5440@gmail.com>
2016-07-11 10:16:23 -0400

    Updated jQuery to autostart the slider

--- image_slider/js/scripts.js
+++ image_slider/js/scripts.js
@@ -1,3 +1,25 @@
+var sliderInt = 1;
+var sliderNext = 2;
+
 $(function() {
-	
+	$("#slider img#1").fadeIn(300);
+	startSlider();
 });
+
+function startSlider() {
+	var count = $("#slider img").size();
+
+	var loop = setInterval(function() {
+		if(sliderNext > count) {
+			sliderNext = 1;
+			sliderInt = 1;
+		}
+		$("#slider img").fadeOut(300);
+		$("#slider img#" + sliderNext).fadeIn(300);
+		sliderInt = sliderNext;
+		sliderNext++;
+
+	}, 3000);
+}
+
+
